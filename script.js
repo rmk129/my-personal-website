0c303751f1365fc177b2fa535f4afc806fe8995e
Romil <romil.m.kothari@gmail.com>
2023-03-12 18:18:10 -0400

    added more stuff

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.id = "welcome"
+h2.textContent = "Welcome to my world";
 document.querySelector("body").appendChild(h2);
+
